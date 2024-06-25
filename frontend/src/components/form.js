9e77ed81dc47d366d405b4bced6c0a19ca9d0bba
Timson974 <t.utepov@hotmail.com>
2024-06-25 10:01:00 +0100

    Correct registration & authorization

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -4,17 +4,19 @@ import config from "../../config/config.js";
 
 export class Form {
     constructor(page) {
-        this.agreeElement = null;
+        this.rememberMe = null;
         this.processElement = null;
         this.page = page;
 
-        const accessToken = localStorage.getItem(Auth.accessTokenKey);
-        if (accessToken) {
-            location.href = '#/signup';
-            return;
-        }
-
         this.fields = [
+
+            // {
+            //     name: 'name',
+            //     id: 'name',
+            //     element: null,
+            //     regex: /^[А-Я][а-я]+(\s[А-Я][а-я]+)?$/,
+            //     valid: false
+            // },
 
             {
                 name: 'email',
@@ -41,10 +43,10 @@ export class Form {
                     valid: false
                 },
                 {
-                    name: 'lastName',
-                    id: 'last-name',
+                    name: 'repeatPassword',
+                    id: 'repeatPassword',
                     element: null,
-                    regex: /^[А-Я][а-я]+\s*$/,
+                    regex: null,
                     valid: false
                 })
         }
@@ -53,6 +55,7 @@ export class Form {
         const that = this;
         this.fields.forEach(item => {
             item.element = document.getElementById(item.id);
+            // console.log(item.name, item.element);
             item.element.onchange = function () {
                 that.validateField.call(that, item, this)
             }
@@ -63,49 +66,100 @@ export class Form {
             that.processForm()
         }
 
-        if (this.page === 'signup') {
-            this.agreeElement = document.getElementById('agree');
-            this.agreeElement.onchange = function () {
+        if (this.page === 'login') {
+            this.rememberMe = document.getElementById('agree');
+            this.rememberMe.onchange = function () {
                 that.validateForm()
             }
         }
     }
 
+    // validateField(field, element) {
+    //     if (!element.value || !element.value.match(field.regex)) {
+    //         element.parentNode.style.borderColor = 'red';
+    //         field.valid = false;
+    //     } else {
+    //         element.parentNode.removeAttribute('style');
+    //         field.valid = true;
+    //     }
+    //     this.validateForm()
+    // }
 
-    validateField(field, element) {
-        if (!element.value || !element.value.match(field.regex)) {
-            element.parentNode.style.borderColor = 'red';
+    validateField(field) {
+        const value = field.element.value;
+        if (!value || !value.match(field.regex)) {
+            field.element.parentNode.style.borderColor = 'red';
             field.valid = false;
         } else {
-            element.parentNode.removeAttribute('style');
+            // field.element.parentNode.style.borderColor = ''; // or use removeAttribute('style')
+            field.element.parentNode.removeAttribute('style');
+
             field.valid = true;
+            console.log(field.name + " is valid"); // Добавьте эту строку для отладки
         }
-        this.validateForm()
+        this.validateForm();
     }
+
+    // validateField(field) {
+    //     const value = field.element.value;
+    //     if (field.name === 'repeatPassword') {
+    //         const passwordValue = this.fields.find(f => f.name === 'password').element.value;
+    //         if (value !== passwordValue) {
+    //             field.element.parentNode.style.borderColor = 'red';
+    //             field.valid = false;
+    //         } else {
+    //             field.element.parentNode.removeAttribute('style');
+    //             field.valid = true;
+    //         }
+    //     } else if (!value || !value.match(field.regex)) {
+    //         field.element.parentNode.style.borderColor = 'red';
+    //         field.valid = false;
+    //     } else {
+    //         field.element.parentNode.removeAttribute('style');
+    //         field.valid = true;
+    //     }
+    //     console.log(field.name + " is valid:", field.valid); // Для отладки
+    //     this.validateForm();
+    // }
+
+
 
     validateForm() {
         const validForm = this.fields.every(item => item.valid);
-        const isValid = this.agreeElement ? this.agreeElement.checked && validForm : validForm;
+        const isValid = this.rememberMe ? this.rememberMe.checked && validForm : validForm;
         if (isValid) {
             this.processElement.removeAttribute('disabled');
         } else {
             this.processElement.setAttribute('disabled', 'disabled')
         }
+
+        console.log(this.fields)
+        console.log(isValid)
         return isValid
     }
+
+    // validateForm() {
+    //     const validForm = this.fields.every(item => item.valid);
+    //     this.processElement.disabled = !validForm;
+    //     console.log("All fields valid:", validForm);
+    //     return validForm;
+    // }
+
 
     async processForm() {
         if (this.validateForm()) {
             const email = this.fields.find(item => item.name === 'email').element.value;
             const password = this.fields.find(item => item.name === 'password').element.value;
+            const [name, lastName] = this.fields.find(item => item.name === 'name').element.value.split('.');
 
             if (this.page === 'signup') {
                 try {
                     const result = await CustomHttp.request(config.host + '/signup', 'POST', {
-                        name: this.fields.find(item => item.name === 'name').element.value,
-                        lastName: this.fields.find(item => item.name === 'lastName').element.value,
+                        name: name,
+                        lastName: lastName,
                         email: email,
                         password: password,
+                        passwordRepeat: this.fields.find(item => item.name === repeatPassword).element.value
                     })
 
                     if (result) {
@@ -135,12 +189,45 @@ export class Form {
                         userId: result.userId,
                         email: email
                     })
-                    location.href = '#/income&expense';
+                    location.href = '#/';
                 }
             } catch (error) {
                 console.log(error);
             }
         }
     }
+
+    // async processForm() {
+    //     if (this.validateForm()) {
+    //         const email = this.fields.find(item => item.name === 'email').element.value;
+    //         const password = this.fields.find(item => item.name === 'password').element.value;
+    //         const name = this.fields.find(item => item.name === 'name').element.value;
+    //
+    //         try {
+    //             const result = await CustomHttp.request(config.host + '/signup', 'POST', {
+    //                 name: name,
+    //                 email: email,
+    //                 password: password,
+    //                 passwordRepeat: this.fields.find(item => item.name === 'repeatPassword').element.value
+    //             });
+    //
+    //             if (result && !result.error) {
+    //                 Auth.setTokens(result.accessToken, result.refreshToken);
+    //                 Auth.setUserInfo({
+    //                     fullName: result.fullName,
+    //                     userId: result.userId,
+    //                     email: email
+    //                 });
+    //                 window.location.href = '/main.html'; // Убедитесь, что путь корректен
+    //             } else {
+    //                 throw new Error(result.message);
+    //             }
+    //         } catch (error) {
+    //             console.error("Registration error:", error);
+    //         }
+    //     }
+    // }
+
 }
+
 
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,5 +1,5 @@
 import {Form} from "./components/form.js";
-import {Chart} from "./components/chart.js";
+import {Main} from "./components/main.js";
 import {Auth} from "./services/auth.js";
 
 export class Router {
@@ -15,8 +15,9 @@ export class Router {
             {
                 route: '#/',
                 title: 'Главная',
-                template: 'templates/index.html',
+                template: 'templates/main.html',
                 load: () => {
+                    new Main();
 
                 }
             },
@@ -24,7 +25,6 @@ export class Router {
                 route: '#/signup',
                 title: 'Создайте аккаунт',
                 template: 'templates/register.html',
-                style: 'styles/bootstrap.min.css',
                 load: () => {
                     new Form('signup');
                 }
@@ -33,7 +33,6 @@ export class Router {
                 route: '#/login',
                 title: 'Вход',
                 template: 'templates/login.html',
-                style: 'styles/bootstrap.min.css',
                 load: () => {
                     new Form('login');
                 }
@@ -47,101 +46,75 @@ export class Router {
                 }
             },
             {
-                route: '#/chart',
+                route: '#/expenses',
                 title: 'Затраты',
                 template: 'templates/expenses.html',
                 load: () => {
-                    new Chart();
-                }
-            },
-            {
-                route: '#/create-income',
-                title: 'Создание Прибыли',
-                template: 'templates/create_income.html',
-                load: () => {
-                    new Chart();
-                }
-            },
-            {
-                route: '#/create-expense',
-                title: 'Создание Затраты',
-                template: 'templates/create_expense.html',
-                load: () => {
-                    new CreateExpense();
-                }
-            },
-            {
-                route: '#/edit-income',
-                title: 'Изменение Прибыли',
-                template: 'templates/edit_income.html',
-                load: () => {
-                    new EditIncome();
-                }
-            },
-            {
-                route: '#/edit-expense',
-                title: 'Изменение Затраты',
-                template: 'templates/edit_expense.html',
-                load: () => {
-                    new EditExpense();
-                }
-            },
-            {
-                route: '#/income&expense',
-                title: 'Общие Прибыли и Затраты',
-                template: 'templates/income&expense.html',
-                load: () => {
-                    new Income&Expense();
-                }
-            },
-            {
-                route: '#/create-income&expense',
-                title: 'Создание Прибыли & Затраты',
-                template: 'templates/create_income&expense.html',
-                load: () => {
-                    new CreateIncome&Expense();
-                }
-            },
-            {
-                route: '#/edit-income&expense',
-                title: 'Изменение Прибыли & Затраты',
-                template: 'templates/edit_income&expense.html',
-                load: () => {
-                    new EditIncome&Expense();
+                    new Expense();
                 }
             }
 
         ]
     }
 
+    // async openRoute() {
+    //     const urlRoute = window.location.hash.split('?')[0];
+    //     if (urlRoute === '#/logout') {
+    //         await Auth.logout();
+    //         window.location.href = '#/';
+    //         return;
+    //     }
+    //
+    //     const newRoute = this.routes.find(item => {
+    //         return item.route === urlRoute;
+    //     })
+    //
+    //     if(!newRoute) {
+    //         window.location.href = '#/login';
+    //         return
+    //     }
+    //
+    //     this.contentElement.innerHTML = await fetch(newRoute.template).then(response => response.text());
+    //     this.titleElement.innerText = newRoute.title;
+    //
+    //     if(urlRoute === '#/login' || urlRoute === '#signup') {
+    //         this.contentElement.classList.add('justify-content-center');
+    //         this.contentElement.classList.add('auth-container');
+    //     } else {
+    //         const userInfo = Auth.getUserInfo();
+    //         const accessToken = localStorage.getItem(Auth.accessTokenKey);
+    //
+    //         if (!userInfo && !accessToken) {
+    //             this.profileElement.innerText = userInfo.name + '' + userInfo.lastName;
+    //             // Load balance data
+    //         } else {
+    //             location.href = '#/login';
+    //         }
+    //     }
+    //     console.log(this.contentElement);
+    //
+    //     newRoute.load();
+    // }
+
     async openRoute() {
         const urlRoute = window.location.hash.split('?')[0];
-        if (urlRoute === '#/logout') {
-            await Auth.logout();
-            window.location.href = '#/';
+        const newRoute = this.routes.find(item => item.route === urlRoute);
+
+        if (!newRoute) {
+            window.location.href = '#/login';
             return;
         }
 
-        const newRoute = this.routes.find(item => {
-            return item.route === urlRoute;
-        })
-
-        if(!newRoute) {
-            window.location.href = '#/';
-            return
-        }
         this.contentElement.innerHTML = await fetch(newRoute.template).then(response => response.text());
-        this.stylesElement.setAttribute('href', newRoute.styles);
         this.titleElement.innerText = newRoute.title;
 
-        const userInfo = Auth.getUserInfo();
-        const accessToken = localStorage.getItem(Auth.accessTokenKey);
-        if (userInfo && accessToken) {
-            this.profileElement.style.display = 'flex';
-            this.profileFullNameElement.innerText = userInfo.fullName;
+        if (urlRoute === '#/login' || urlRoute === '#/signup') {
+            this.contentElement.className = 'd-flex justify-content-center auth-container';
         } else {
-            this.profileElement.style.display = 'none';
+            this.contentElement.className = ''; // Clear previous class if any
         }
+
         newRoute.load();
     }
+
 }
